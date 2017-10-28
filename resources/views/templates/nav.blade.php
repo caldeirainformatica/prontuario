cc33dcba683ffdbce21a56c5510ae5178edf4b02
Ricardo Caldeira <ricardo@cpdsi.com.br>
2017-10-28 15:34:44 -0200

    Navbar

--- resources/views/templates/nav.blade.php
+++ resources/views/templates/nav.blade.php
@@ -14,12 +14,16 @@
 		<a href="/" class="brand-logo"><img src="{{asset('img/logo.png')}}" class="logo"></a>
 		<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
 		<ul class="right hide-on-med-and-down">
-			<li><a href="/aluno">Aluno</a></li>
-			<li><a href="/supervisor">Supervisor</a></li>
+			<li><a href="{{route('aluno.index')}}">Aluno</a></li>
+			<li><a href="{{route('supervisor.index')}}">Supervisor</a></li>
+			<li><a href="{{route('triagem.index')}}">Triagem</a></li>
+
 		</ul>
 		<ul class="side-nav" id="mobile-demo">
-			<li><a href="/aluno">Aluno</a></li>
-			<li><a href="/supervisor">Supervisor</a></li>
+			<li><a href="{{route('aluno.index')}}">Aluno</a></li>
+			<li><a href="{{route('supervisor.index')}}">Supervisor</a></li>
+			<li><a href="{{route('triagem.index')}}">Triagem</a></li>
+
 		</ul>
 	</div>
 </nav>
